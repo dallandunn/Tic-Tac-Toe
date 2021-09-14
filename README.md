d6d21635c91123fe843048e6997b7648f6682bc8
Dallan Dumm <41130610+dallandunn@users.noreply.github.com>
2021-09-14 10:15:53 -0400

    Add functionality for two players

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tic Tac Toe Terminal Game
+
+This is a simple tic tac toe terminal game created in python. 
+
+There is functionality for 1 or 2 players.
---
+++ tic_tac_toe.py
@@ -0,0 +1,144 @@
+import random
+import time
+
+class GameBoard:
+    def __init__(self):
+        self.values = [str(i) for i in range(1, 10)]
+
+
+    def show(self):
+        self.board = '\n'
+        for i in range(1,10):
+            if i % 3 != 0:
+                self.board += self.values[i-1] + ' | '
+            else:
+                self.board += self.values[i-1] + '\n'
+                if i !=9:
+                    self.board += ('-' * 10) + '\n'
+        self.board += '\n'
+        print(self.board, end='\r')
+
+    def update(self, value, location):
+        self.values[location - 1] = value
+
+    def reset(self):
+        print('Game Board has been reset.')
+        self.__init__()
+
+class Game:
+    def __init__(self, GameBoard):
+        self.Board = GameBoard
+        self.game_status = True
+        print('Welcome to Tick Tac Toe!')
+        input('Press [Enter] to start')
+        self.Board.show()
+    
+    def game_over(self):
+        replay = input('Play Again? (Y/N)')
+        if replay.lower() == 'y':
+            self.Board.reset()
+            self.Board.show()
+            self.game_status = True
+        else:
+            print('Thanks for playing!')
+        
+    def check_game_end(self):
+        # check for 3 in rows
+        for i in range(0, 9, 3):
+            if self.Board.values[i] == self.Board.values[i+1] == self.Board.values[i+2]:
+                print(f'Game Over! {self.Board.values[i]} won!')
+                self.game_status = False
+                self.game_over()
+
+        # check for 3 in cols
+        for i in range(3):
+            if self.Board.values[i] == self.Board.values[i+3] == self.Board.values[i+6]:
+                print(f'Game Over! {self.Board.values[i]} won!')
+                self.game_status = False
+                self.game_over()
+         
+        # check for 3 diagnal
+        if self.Board.values[0] == self.Board.values[4] == self.Board.values[8]:
+            print(f'Game Over! {self.Board.values[0]} won!')
+            self.game_status = False
+            self.game_over()
+
+        if self.Board.values[2] == self.Board.values[4] == self.Board.values[6]:
+            print(f'Game Over! {self.Board.values[2]} won!')
+            self.game_status = False
+            self.game_over()
+
+        # check if no spaces left
+        if not any([value.isnumeric() for value in self.Board.values]):
+            print('Game Tied! No spaces left.')
+            self.game_status = False
+            self.game_over()
+
+    def turn(self, player_symbol, player_choice):
+        if self.game_status:
+            self.Board.update(player_symbol, player_choice)
+            self.Board.show()
+            self.check_game_end()
+        
+    def player_turn(self, player_symbol):
+        player_input = ''
+        print(f'Player {player_symbol} turn')
+        # ensure that player input is an acceptable value
+        while player_input not in range(1, 10):
+            player_input = input('Choose a space: ')
+            if player_input.isnumeric():
+                if int(player_input) > 0 and int(player_input) < 10:
+                    try:
+                        self.Board.values[int(player_input) - 1].isnumeric()
+                        self.turn(player_symbol, int(player_input))
+                        break
+                    except IndexError:
+                        print('Please enter a number between 1 and 9.')
+                    except:
+                        print('This space has already been chosen. Please choose another space.')
+                else:
+                    print('Please enter a number between 1 and 9.')
+            else:
+                print('Please enter a number.')
+    
+    def play(self, player1_symbol='X', player2_symbol='O'):
+        # set up number of players
+        num_players = ''
+        while num_players != 1 and num_players != 2:
+            num_players = input('How many players?')
+            try:
+                num_players = int(num_players)
+                if num_players > 2 or num_players < 1:
+                    print('This game can only be played with 1 or 2 players.')
+            except:
+                print('Please enter either 1 or 2.')
+
+        # for 1 player 
+        if num_players == 1:
+            while self.game_status:
+                # player turn
+                self.player_turn(player1_symbol)
+
+                # CPU turn
+                print('CPU turn...')
+                time.sleep(1.5)
+                
+                # find all spaces that are not taken and randomly choose one of those spaces
+                available_spaces = [space for space in self.Board.values if space.isnumeric()]
+                cpu_choice = random.choice(available_spaces)
+
+                self.turn(player2_symbol, int(cpu_choice))
+        
+        # for 2 players
+        elif num_players == 2:
+            while self.game_status:
+                # player 1 turn
+                self.player_turn(player1_symbol)
+
+                # player 2 turn
+                self.player_turn(player2_symbol)
+
+Board = GameBoard()
+TicTacToe = Game(Board)
+TicTacToe.play()
+
